222f2690bb9f4e0e89e0925ed2f29e14c80f6429
Leonardo Cunha <lalmcunha@gmail.com>
2020-05-25 21:09:22 -0300

    app nodejs terminada

--- curso-node/server.js
+++ curso-node/server.js
@@ -1,28 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o APp
 const app = express();
+app.use(express.json()); // enviar dados para a aplicação em formato json
+app.use(cors());
 
 // Iniciando o DB
 mongoose.connect(
     'mongodb://localhost:27017/node-api-rs-starter', 
-    { useNewUrlParser: true }
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
 // Primeira rota
-app.get('/', (req, res) => {
-    Product.create( {
-        title: 'React Native',
-        description: 'primeiro teste',
-        url: 'google.com',
-    })
-    
-    return res.send('Hello');
-})
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
